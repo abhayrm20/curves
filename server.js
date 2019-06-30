bab073c42b9a8f75e0fab09fa3431ed4cf75c33b
abhayrm20 <abhayrm20@gmail.com>
2019-06-30 11:22:56 +0530

    renamed functions.html

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'functions.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
  
 });
 
