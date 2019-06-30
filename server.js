4c9755d7ed75a8202663b615fb938f8cff9a5d5a
abhayrm20 <abhayrm20@gmail.com>
2019-06-30 11:37:02 +0530

    modified port

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'functions.html'));
  
 });
 
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log(`Server listening on port ${port}!`);
 });
